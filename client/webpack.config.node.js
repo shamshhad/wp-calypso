cc653e8e49445d8b9759bba4ca666adb2cbfcc2e
Sergio Cinos <sergio.cinos@gmail.com>
2020-10-22 11:24:38 +0200

    Bundle packages in the monorepo to

--- client/webpack.config.node.js
+++ client/webpack.config.node.js
@@ -47,6 +47,7 @@ const commitSha = process.env.hasOwnProperty( 'COMMIT_SHA' ) ? process.env.COMMI
  *
  * @returns {Array} list of externals
  */
+
 function getExternals() {
 	return [
 		// Don't bundle any node_modules, both to avoid a massive bundle, and problems
@@ -68,10 +69,8 @@ function getExternals() {
 
 				/^calypso\//,
 
-				// Packages in the monorepo that have a `calypso:src` field
-				...packagesInMonorepo()
-					.filter( ( pkg ) => pkg[ 'calypso:src' ] )
-					.map( ( pkg ) => pkg.name ),
+				// Packages in the monorepo
+				...packagesInMonorepo().map( ( pkg ) => pkg.name ),
 			],
 		} ),
 		// Some imports should be resolved to runtime `require()` calls, with paths relative

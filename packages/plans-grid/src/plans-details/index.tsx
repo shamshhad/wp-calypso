e0228f72f866a3e66165dd2b4da2351b5f621c52
Andrew Serong <andrewserong@users.noreply.github.com>
2020-10-26 18:39:58 +1100

    Add loading state UI for detailed comparison features

--- packages/plans-grid/src/plans-details/index.tsx
+++ packages/plans-grid/src/plans-details/index.tsx
@@ -34,50 +34,69 @@ const PlansDetails: React.FunctionComponent< Props > = ( { onSelect } ) => {
 	const prices = useSelect( ( select ) => select( PLANS_STORE ).getPrices() );
 	const supportedPlans = useSelect( ( select ) => select( PLANS_STORE ).getSupportedPlans() );
 
+	const isLoading = ! supportedPlans?.length;
+
 	return (
 		<div className="plans-details">
 			<table className="plans-details__table">
 				<thead>
 					<tr className="plans-details__header-row">
 						<th>{ __( 'Feature', __i18n_text_domain__ ) }</th>
-						{ supportedPlans.map( ( plan ) => (
-							<th key={ plan.storeSlug }>{ plan.title }</th>
-						) ) }
+						{ isLoading
+							? [ 1, 2, 3, 4, 5 ].map( ( placeholder ) => (
+									<th key={ placeholder }>
+										<span className="plans-details__placeholder">{ '' }</span>
+									</th>
+							  ) )
+							: supportedPlans.map( ( plan ) => <th key={ plan.storeSlug }>{ plan.title }</th> ) }
 					</tr>
 				</thead>
 
-				{ featuresByType.map( ( featureType ) => (
-					<tbody key={ featureType.id }>
-						{ featureType.name && (
-							<tr className="plans-details__header-row">
-								<th colSpan={ 6 }>{ featureType.name }</th>
-							</tr>
-						) }
-						{ featureType.features?.map( ( feature: string, i ) => (
+				{ isLoading
+					? [ 1, 2, 3, 4, 5 ].map( ( placeholder, i ) => (
 							<tr className="plans-details__feature-row" key={ i }>
-								<th>{ features[ feature ].name }</th>
-								{ supportedPlans.map( ( plan, j ) => (
-									<td key={ j }>
-										{ plans[ plan.storeSlug ].featuresSlugs?.[ feature ] ? (
-											<>
-												{ /* eslint-disable-next-line wpcalypso/jsx-classname-namespace */ }
-												<span className="hidden">{ __( 'Available', __i18n_text_domain__ ) }</span>
-												{ TickIcon }
-											</>
-										) : (
-											<>
-												{ /* eslint-disable-next-line wpcalypso/jsx-classname-namespace */ }
-												<span className="hidden">
-													{ __( 'Unavailable', __i18n_text_domain__ ) }
-												</span>
-											</>
-										) }
-									</td>
+								<th key={ placeholder }>
+									<span className="plans-details__placeholder--wide">{ '' }</span>
+								</th>
+								{ [ 1, 2, 3, 4, 5 ].map( ( j ) => (
+									<td key={ j }></td>
 								) ) }
 							</tr>
-						) ) }
-					</tbody>
-				) ) }
+					  ) )
+					: featuresByType.map( ( featureType ) => (
+							<tbody key={ featureType.id }>
+								{ featureType.name && (
+									<tr className="plans-details__header-row">
+										<th colSpan={ 6 }>{ featureType.name }</th>
+									</tr>
+								) }
+								{ featureType.features?.map( ( feature: string, i ) => (
+									<tr className="plans-details__feature-row" key={ i }>
+										<th>{ features[ feature ].name }</th>
+										{ supportedPlans.map( ( plan, j ) => (
+											<td key={ j }>
+												{ plans[ plan.storeSlug ].featuresSlugs?.[ feature ] ? (
+													<>
+														{ /* eslint-disable-next-line wpcalypso/jsx-classname-namespace */ }
+														<span className="hidden">
+															{ __( 'Available', __i18n_text_domain__ ) }
+														</span>
+														{ TickIcon }
+													</>
+												) : (
+													<>
+														{ /* eslint-disable-next-line wpcalypso/jsx-classname-namespace */ }
+														<span className="hidden">
+															{ __( 'Unavailable', __i18n_text_domain__ ) }
+														</span>
+													</>
+												) }
+											</td>
+										) ) }
+									</tr>
+								) ) }
+							</tbody>
+					  ) ) }
 
 				<tbody>
 					<tr className="plans-details__header-row">
